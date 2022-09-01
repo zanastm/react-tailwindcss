37c2e9133924d7d6b9e8e2bfa379ba86c4533931
zanastm <89668280+zanastm@users.noreply.github.com>
2022-09-01 08:49:37 +0200

    Changes to be committed: 	modified:   README.md 	new file:   react-tailwindcss/.gitignore 	new file:   react-tailwindcss/package-lock.json 	new file:   react-tailwindcss/package.json 	new file:   react-tailwindcss/postcss.config.js 	new file:   react-tailwindcss/public/favicon.ico 	new file:   react-tailwindcss/public/index.html 	new file:   react-tailwindcss/public/logo192.png 	new file:   react-tailwindcss/public/logo512.png 	new file:   react-tailwindcss/public/manifest.json 	new file:   react-tailwindcss/public/robots.txt 	new file:   react-tailwindcss/src/App.js 	new file:   react-tailwindcss/src/assets/._cyber-bg.png 	new file:   react-tailwindcss/src/assets/._support.jpeg 	new file:   react-tailwindcss/src/assets/cyber-bg.png 	new file:   react-tailwindcss/src/assets/support.jpeg 	new file:   react-tailwindcss/src/components/About.jsx 	new file:   react-tailwindcss/src/components/AllInOne.jsx 	new file:   react-tailwindcss/src/components/Footer.jsx 	new file:   react-tailwindcss/src/components/Hero.jsx 	new file:   react-tailwindcss/src/components/Navbar.jsx 	new file:   react-tailwindcss/src/components/Pricing.jsx 	new file:   react-tailwindcss/src/components/Support.jsx 	new file:   react-tailwindcss/src/index.css 	new file:   react-tailwindcss/src/index.js 	new file:   react-tailwindcss/tailwind.config.js

---
+++ react-tailwindcss/src/App.js
@@ -0,0 +1,12 @@
+import NavBar from './components/Navbar.jsx'
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-white bg-gray-300">hey!!</h1>
+            <NavBar />
+        </div>
+    )
+}
+
+export default App;
